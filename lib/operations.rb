8943da277b1b64d4ab98fb3a51115b408191b1ad
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-20 04:09:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,7 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
     "true"
+  end  
 end
 
 
@@ -8,6 +9,7 @@ end
 def not_safe?(speed)
 	speed < 40 || speed > 60 ? "true" : "false"
 end
+
 	
 unsafe?(50)
 not_safe?(90)
